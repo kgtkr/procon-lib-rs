317a3400a3024188730696e21e04906c4379d733
kgtkr <kgtkr.jp@gmail.com>
2018-02-20 00:42:52 +0900

    綺麗にした(まだ未完成)

--- src/dijsktra.rs
+++ src/dijsktra.rs
@@ -1,97 +1,105 @@
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Debug, Clone)]
+pub struct Edges {
+  to: Node,
+  cost: i32,
+}
+
+#[derive(PartialEq, Debug, Clone)]
 pub struct Node {
-  pub edges: Vec<(usize, f64)>,
-  pub done: bool,
-  pub cost: f64,
-  pub id: usize,
-  pub previous_node: Option<usize>,
+  edges: Vec<Edges>,
+  done: bool,
+  cost: Option<i32>,
 }
 
-impl Node {
-  pub fn new(id: usize) -> Node {
-    Node {
-      edges: Vec::new(),
-      done: false,
-      cost: -1.0,
-      id: id,
-      previous_node: None,
-    }
-  }
-
-  pub fn add_node(&mut self, id: usize, cost: f64) {
-    self.edges.push((id, cost));
-  }
-}
-
-pub fn min_route(mut nodes: Vec<Node>) -> (f64, Vec<Node>) {
-  nodes[0].cost = 0.0;
-
-  loop {
-    let mut process_node: Option<usize> = None;
-
-    for node in &nodes {
-      // 訪問済み or まだコストが未設定
-      if node.done || node.cost < 0.0 {
-        continue;
-      }
-
-      match process_node {
-        Option::None => process_node = Some(node.id),
-        Option::Some(pn) => {
-          if node.cost < nodes[pn].cost {
-            process_node = Some(node.id);
-          }
-        }
-      }
-    }
-
-    match process_node {
-      Option::Some(pn) => {
-        nodes[pn].done = true;
-
-        for edge in nodes[pn].edges.clone() {
-          let node = edge.0;
-          let cost = nodes[pn].cost + edge.1;
-
-          // コストが未設定 or コストの少ない経路がある場合はアップデート
-          let needs_update = (nodes[node].cost < 0.0) || (nodes[node].cost > cost);
-          if needs_update {
-            nodes[node].cost = cost;
-            nodes[node].previous_node = Some(pn);
-          }
-        }
-      }
-      None => {
-        break;
-      }
-    }
-  }
-
-  let goal_node = nodes.last().unwrap();
-
-  let mut path: Vec<Node> = Vec::new();
-  let mut current_node = goal_node;
-  loop {
-    let next_node = current_node.previous_node;
-    match next_node {
-      Option::None => {
-        break;
-      }
-      Option::Some(next_node) => {
-        path.push(nodes[next_node].clone());
-        current_node = &nodes[next_node];
-      }
-    }
-  }
-
-  let sum = path
+//スタートのコストを0とすること
+pub fn dijsktra(nodes: Vec<Node>) -> Vec<Node> {
+  let done = nodes
     .clone()
     .into_iter()
-    .filter_map(|x| {
-      x.previous_node
-        .map(|id| x.edges.iter().find(|x| x.0 == id).unwrap().1)
+    .enumerate()
+    .filter(|&(_, ref x)| !x.done)
+    .fold(None, |min, (i, x)| match min {
+      Option::None => Some((i, x)),
+      Option::Some((_, min)) => match (min.cost, x.cost) {
+        (Some(min_cost), Some(x_cost)) => {
+          if min_cost > x_cost {
+            Some((i, x))
+          } else {
+            None
+          }
+        }
+        _ => None,
+      },
     })
-    .sum::<f64>() + path[0].edges.last().unwrap().1;
+    .map(|(i, done_node)| {
+      (
+        i,
+        Node {
+          done: true,
+          edges: done_node
+            .edges
+            .clone()
+            .into_iter()
+            .map(|edge| {
+              let cost = done_node.cost.unwrap() + edge.cost;
+              if edge.to.cost.map(|to_cost| cost < to_cost).unwrap_or(true) {
+                Edges {
+                  to: Node {
+                    cost: Some(cost),
+                    ..edge.to
+                  },
+                  ..edge
+                }
+              } else {
+                edge
+              }
+            })
+            .collect::<Vec<_>>(),
+          ..done_node
+        },
+      )
+    });
+  if let Some((i, done)) = done {
+    dijsktra({
+      let mut n = nodes;
+      n[i] = done;
+      n
+    })
+  } else {
+    nodes
+  }
+}
 
-  (sum, path)
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test1() {
+    let mut map = vec![
+      vec![true, false, true, false, true],
+      vec![true, false, true, true, true],
+      vec![true, false, true, false, true],
+      vec![true, true, true, false, true],
+      vec![true, false, true, false, true],
+    ];
+    assert_eq!(
+      Some(vec![
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (3, 1),
+        (3, 2),
+        (2, 2),
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (2, 4),
+        (3, 4),
+        (4, 4),
+      ]),
+      bfs(&map)
+    );
+  }
 }
