facac352da9ce9e70dd755df4c4e5f766626c84e
kgtkr <kgtkr.jp@gmail.com>
2018-02-20 19:10:04 +0900

    とりあえず出来た

--- src/dijsktra.rs
+++ src/dijsktra.rs
@@ -1,14 +1,28 @@
 #[derive(PartialEq, Debug, Clone)]
-pub struct Edges {
-  to: usize,
-  cost: i32,
+pub struct Edge {
+  pub to: usize,
+  pub cost: i32,
+}
+
+impl Node {
+  pub fn new(edges: Vec<Edge>) -> Node {
+    Node {
+      edges: edges,
+      done: false,
+      cost: None,
+    }
+  }
+
+  pub fn set_start(&mut self) {
+    self.cost = Some(0);
+  }
 }
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct Node {
-  edges: Vec<Edges>,
-  done: bool,
-  cost: Option<i32>,
+  pub edges: Vec<Edge>,
+  pub done: bool,
+  pub cost: Option<i32>,
 }
 
 //スタートのコストを0とすること
@@ -18,18 +32,16 @@ pub fn dijsktra(nodes: &mut Vec<Node>) {
     .into_iter()
     .enumerate()
     .filter(|&(_, ref x)| !x.done)
-    .fold(None, |min, (i, x)| match min {
-      Option::None => Some((i, x)),
-      Option::Some((_, min)) => match (min.cost, x.cost) {
-        (Some(min_cost), Some(x_cost)) => {
-          if min_cost > x_cost {
-            Some((i, x))
-          } else {
-            None
-          }
-        }
-        _ => None,
-      },
+    .fold(None::<(usize, Node)>, |min, x| {
+      Some(
+        min
+          .map(|min| match (x.1.cost, min.1.cost) {
+            (Option::Some(_), Option::None) => x.clone(),
+            (Option::Some(x_cost), Option::Some(min_cost)) if x_cost < min_cost => x.clone(),
+            _ => min,
+          })
+          .unwrap_or(x),
+      )
     })
     .map(|(i, _)| i);
 
@@ -56,30 +68,54 @@ mod tests {
 
   #[test]
   fn test1() {
-    let mut map = vec![
-      vec![true, false, true, false, true],
-      vec![true, false, true, true, true],
-      vec![true, false, true, false, true],
-      vec![true, true, true, false, true],
-      vec![true, false, true, false, true],
-    ];
-    assert_eq!(
-      Some(vec![
-        (0, 0),
-        (1, 0),
-        (2, 0),
-        (3, 0),
-        (3, 1),
-        (3, 2),
-        (2, 2),
-        (1, 2),
-        (1, 3),
-        (1, 4),
-        (2, 4),
-        (3, 4),
-        (4, 4),
+    let mut nodes = vec![
+      Node::new(vec![Edge { to: 2, cost: 10 }, Edge { to: 1, cost: 1 }]),
+      Node::new(vec![Edge { to: 3, cost: 2 }]),
+      Node::new(vec![
+        Edge { to: 1, cost: 1 },
+        Edge { to: 3, cost: 3 },
+        Edge { to: 4, cost: 1 },
       ]),
-      bfs(&map)
+      Node::new(vec![Edge { to: 0, cost: 7 }, Edge { to: 4, cost: 2 }]),
+      Node::new(vec![]),
+    ];
+
+    nodes[0].set_start();
+    dijsktra(&mut nodes);
+
+    assert_eq!(
+      vec![
+        Node {
+          edges: vec![Edge { to: 2, cost: 10 }, Edge { to: 1, cost: 1 }],
+          done: true,
+          cost: Some(0),
+        },
+        Node {
+          edges: vec![Edge { to: 3, cost: 2 }],
+          done: true,
+          cost: Some(1),
+        },
+        Node {
+          edges: vec![
+            Edge { to: 1, cost: 1 },
+            Edge { to: 3, cost: 3 },
+            Edge { to: 4, cost: 1 },
+          ],
+          done: true,
+          cost: Some(10),
+        },
+        Node {
+          edges: vec![Edge { to: 0, cost: 7 }, Edge { to: 4, cost: 2 }],
+          done: true,
+          cost: Some(3),
+        },
+        Node {
+          edges: vec![],
+          done: true,
+          cost: Some(5),
+        },
+      ],
+      nodes
     );
   }
 }
