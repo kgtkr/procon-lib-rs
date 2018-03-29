bad1a0294f9fbc8108ed379feeb42a3f63aad875
kgtkr <kgtkr.jp@gmail.com>
2018-03-29 22:50:11 +0900

    bound関数

--- src/bound.rs
+++ src/bound.rs
@@ -1,8 +1,10 @@
 //参考 http://www.pandanoir.info/entry/2015/12/26/190000
-//xを境に
-//xと同じ: upper_bound-lower_bound
-//xより小さい: lower_bound
-//xより大きい: len-upper_bound
+//(小さい,同じ,大きい)
+pub fn bound<T: Ord>(vec: &Vec<T>, val: &T) -> (usize, usize, usize) {
+  let lb = lower_bound(vec, 0, vec.len(), val);
+  let ub = upper_bound(vec, 0, vec.len(), val);
+  (lb, ub - lb, vec.len() - ub)
+}
 
 //vecは昇順ソート済み
 //以上
@@ -52,17 +54,22 @@ mod tests {
 
     assert_eq!(0, lower_bound(&vec, 0, len, &-2));
     assert_eq!(0, upper_bound(&vec, 0, len, &-2));
+    assert_eq!((0, 0, 10), bound(&vec, &-2));
 
     assert_eq!(0, lower_bound(&vec, 0, len, &-1));
     assert_eq!(1, upper_bound(&vec, 0, len, &-1));
+    assert_eq!((0, 1, 9), bound(&vec, &-1));
 
     assert_eq!(1, lower_bound(&vec, 0, len, &0));
     assert_eq!(4, upper_bound(&vec, 0, len, &0));
+    assert_eq!((1, 3, 6), bound(&vec, &0));
 
     assert_eq!(9, lower_bound(&vec, 0, len, &9));
     assert_eq!(10, upper_bound(&vec, 0, len, &9));
+    assert_eq!((9, 1, 0), bound(&vec, &9));
 
     assert_eq!(10, lower_bound(&vec, 0, len, &10));
     assert_eq!(10, upper_bound(&vec, 0, len, &10));
+    assert_eq!((10, 0, 0), bound(&vec, &10));
   }
 }
