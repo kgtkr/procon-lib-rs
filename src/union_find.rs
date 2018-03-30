9c0fcee20cc89f84b3769550b69b3662b8a71e4a
kgtkr <kgtkr.jp@gmail.com>
2018-03-30 23:58:38 +0900

    uf new

--- src/union_find.rs
+++ src/union_find.rs
@@ -6,3 +6,14 @@ pub struct UnionFindNode {
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct UnionFind(pub Vec<UnionFindNode>);
+
+impl UnionFind {
+  pub fn new(size: usize) -> UnionFind {
+    let mut vec = Vec::new();
+    for i in 0..size {
+      vec.push(UnionFindNode { par: i, rank: 0 });
+    }
+
+    UnionFind(vec)
+  }
+}
