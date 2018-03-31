940866cc99f4e1756ea948f92052144623b58207
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 11:23:15 +0900

    バグ修正、テスト追加

--- src/kruskal.rs
+++ src/kruskal.rs
@@ -2,7 +2,7 @@ use graph::FlatGraph;
 use union_find::UnionFind;
 
 fn kruskal(FlatGraph(node_len, mut edges): FlatGraph) -> FlatGraph {
-  edges.sort_by_key(|&(_, _, x)| x);
+  edges.sort_by(|&(_, _, ref a), &(_, _, ref b)| a.cmp(b));
   let mut uf = UnionFind::new(node_len);
   let mut res = FlatGraph(node_len, Vec::new());
   for (from, to, cost) in edges {
@@ -12,4 +12,40 @@ fn kruskal(FlatGraph(node_len, mut edges): FlatGraph) -> FlatGraph {
     }
   }
   res
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test1() {
+    assert_eq!(
+      FlatGraph(
+        7,
+        vec![
+          (0, 2, 1),
+          (3, 5, 1),
+          (1, 2, 2),
+          (2, 3, 3),
+          (3, 6, 5),
+          (4, 5, 5),
+        ]
+      ),
+      kruskal(FlatGraph(
+        7,
+        vec![
+          (0, 2, 1),
+          (1, 2, 2),
+          (1, 4, 10),
+          (2, 3, 3),
+          (2, 5, 7),
+          (3, 5, 1),
+          (3, 6, 5),
+          (4, 5, 5),
+          (5, 6, 8),
+        ]
+      ))
+    );
+  }
 }
