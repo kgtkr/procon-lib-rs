981a8d6731e307ad20f1540cbc955f91d0104c35
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 11:07:39 +0900

    kruskal

--- src/kruskal.rs
+++ src/kruskal.rs
@@ -1,5 +1,15 @@
 use graph::FlatGraph;
+use union_find::UnionFind;
 
-fn kruskal(FlatGraph(len, mut edges): FlatGraph) {
+fn kruskal(FlatGraph(node_len, mut edges): FlatGraph) -> FlatGraph {
   edges.sort_by_key(|&(_, _, x)| x);
+  let mut uf = UnionFind::new(node_len);
+  let mut res = FlatGraph(node_len, Vec::new());
+  for (from, to, cost) in edges {
+    if !uf.same(from, to) {
+      uf.unite(from, to);
+      res.1.push((from, to, cost));
+    }
+  }
+  res
 }
