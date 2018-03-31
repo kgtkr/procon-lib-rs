c2a9e8db2b8cbb324f3c5b73789183ac845f858c
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 13:13:17 +0900

    rename

--- src/prime.rs
+++ src/prime.rs
@@ -1,5 +1,5 @@
 //max以下の素数列挙
-pub fn prime(max: usize) -> Vec<usize> {
+pub fn divisor(max: usize) -> Vec<usize> {
   let sqrt_max = (max as f64).sqrt();
   let mut prime_list = Vec::new();
 
@@ -24,7 +24,7 @@ mod tests {
 
   #[test]
   fn test1() {
-    assert_eq!(vec![2, 3, 5, 7], prime(10));
-    assert_eq!(vec![2, 3, 5, 7, 11], prime(11));
+    assert_eq!(vec![2, 3, 5, 7], divisor(10));
+    assert_eq!(vec![2, 3, 5, 7, 11], divisor(11));
   }
 }
