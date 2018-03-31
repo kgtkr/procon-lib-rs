233eaa37b2d68f03310ef53a55318c6f4e0208ec
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 13:32:28 +0900

    復元、テスト追加

--- src/prime.rs
+++ src/prime.rs
@@ -1,5 +1,19 @@
+//nの約数列挙
+pub fn divisor(n: usize) -> Vec<usize> {
+  let mut res = Vec::new();
+  for i in (1..).take_while(|x| x * x <= n) {
+    if n % i == 0 {
+      res.push(i);
+      if i != n / i {
+        res.push(n / i);
+      }
+    }
+  }
+  res
+}
+
 //max以下の素数列挙
-pub fn divisor(max: usize) -> Vec<usize> {
+pub fn prime(max: usize) -> Vec<usize> {
   let sqrt_max = (max as f64).sqrt();
   let mut prime_list = Vec::new();
 
@@ -23,8 +37,18 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test1() {
-    assert_eq!(vec![2, 3, 5, 7], divisor(10));
-    assert_eq!(vec![2, 3, 5, 7, 11], divisor(11));
+  fn test_prime() {
+    assert_eq!(vec![2, 3, 5, 7], prime(10));
+    assert_eq!(vec![2, 3, 5, 7, 11], prime(11));
+  }
+
+  #[test]
+  fn test_divisor() {
+    assert_eq!(vec![1, 10, 2, 5], divisor(10));
+    assert_eq!(
+      vec![1, 150, 2, 75, 3, 50, 5, 30, 6, 25, 10, 15],
+      divisor(150)
+    );
+    assert_eq!(vec![1, 9, 3], divisor(9));
   }
 }
