ce285f1be20891927659d262df2c1a0e2cc2b89e
tkr <kgtkr.jp@gmail.com>
2018-04-02 20:00:03 +0900

    バグ修正

--- src/warshall_floyd.rs
+++ src/warshall_floyd.rs
@@ -1,34 +1,22 @@
 use graph;
+use std::cmp::min;
 
-pub fn warshall_floyd(graph::ListGraph(nodes): graph::ListGraph) -> Vec<Vec<Option<graph::Cost>>> {
-  //初期化
-  let len = nodes.len();
-  let mut vec = {
-    let mut vec = Vec::with_capacity(len);
-    let mut vec2 = Vec::with_capacity(len);
-    vec2.resize(len, None);
-    vec.resize(len, vec2);
-    for (node_id, node) in nodes.into_iter().enumerate() {
-      vec[node_id][node_id] = Some(0);
-      for (_, edge_to, edge_cost) in node {
-        vec[node_id][edge_to] = Some(edge_cost);
-      }
-    }
-    vec
-  };
-
-  for k in 0..len {
-    for i in 0..len {
-      for j in 0..len {
+pub fn warshall_floyd(
+  graph::MatrixGraph(mut vec): graph::MatrixGraph,
+) -> Vec<Vec<Option<graph::Cost>>> {
+  for k in 0..vec.len() {
+    for i in 0..vec.len() {
+      for j in 0..vec.len() {
         let now = vec[i][j];
         let new = match (vec[i][k], vec[k][j]) {
           (Some(a), Some(b)) => Some(a + b),
           _ => None,
         };
         vec[i][j] = match (now, new) {
-          (Some(now), Some(new)) if now > new => Some(new),
-          (None, Some(_)) => new,
-          _ => now,
+          (Some(a), Some(b)) => Some(min(a, b)),
+          (None, Some(x)) => Some(x),
+          (Some(x), None) => Some(x),
+          (None, None) => None,
         };
       }
     }
@@ -43,24 +31,23 @@ mod tests {
 
   #[test]
   fn test1() {
-    let mut graph = vec![
+    let mut graph = graph::MatrixGraph::from(graph::ListGraph::from(vec![
       vec![(2, 10), (1, 1)],
       vec![(3, 2)],
       vec![(1, 1), (3, 3), (4, 1)],
       vec![(0, 7), (4, 2)],
       vec![],
-    ];
-    let min = warshall_floyd(graph.into());
+    ]));
 
     assert_eq!(
-      min,
       vec![
-        vec![Some(0), Some(1), Some(10), Some(3), Some(5)],
-        vec![Some(9), Some(0), Some(19), Some(2), Some(4)],
-        vec![Some(10), Some(1), Some(0), Some(3), Some(1)],
-        vec![Some(7), Some(8), Some(17), Some(0), Some(2)],
-        vec![None, None, None, None, Some(0)],
-      ]
+        vec![Some(10), Some(1), Some(10), Some(3), Some(5)],
+        vec![Some(9), Some(10), Some(19), Some(2), Some(4)],
+        vec![Some(10), Some(1), Some(20), Some(3), Some(1)],
+        vec![Some(7), Some(8), Some(17), Some(10), Some(2)],
+        vec![None, None, None, None, None],
+      ],
+      warshall_floyd(graph)
     );
   }
 }
