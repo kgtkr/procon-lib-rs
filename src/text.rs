cf99812ba1fddcb79cd5cd60b3e62a41e0bdff14
tkr <kgtkr.jp@gmail.com>
2018-06-11 20:45:04 +0900

    id2char

--- src/text.rs
+++ src/text.rs
@@ -2,6 +2,10 @@ pub fn char2id(c: char) -> usize {
   c as usize - 'a' as usize
 }
 
+pub fn id2char(id: usize) -> char {
+  (id + 'a' as usize) as u8 as char
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -10,5 +14,11 @@ mod tests {
   fn char2id_test() {
     assert_eq!(0, char2id('a'));
     assert_eq!(1, char2id('b'));
+  }
+
+  #[test]
+  fn id2char_test() {
+    assert_eq!('a', id2char(0));
+    assert_eq!('b', id2char(1));
   }
 }
