e8af79d03b173d86edb848f957cf152a7c7765f7
tkr <kgtkr.jp@gmail.com>
2018-06-24 17:53:14 +0900

    min

--- src/maxmin.rs
+++ src/maxmin.rs
@@ -8,6 +8,16 @@ macro_rules! max {
     max($x,max!($($xs)+))
   };
 }
+
+macro_rules! min {
+  ($x:expr) => {
+    $x
+  };
+  ($x:expr, $($xs:tt)+) => {
+    min($x,min!($($xs)+))
+  };
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -16,6 +26,16 @@ mod tests {
     assert_eq!(10, max!(10));
     assert_eq!(10, max!(1, 2, 10));
     assert_eq!(10, max!(2, 10, 1));
-    assert_eq!(10, max!(10, 2, 1));
+    assert_eq!(10, max!(10, 1, 2));
+    assert_eq!(10, max!(2, 10, 1, 2, 5, 8, 7));
+  }
+
+  #[test]
+  fn min_test() {
+    assert_eq!(1, min!(1));
+    assert_eq!(1, min!(1, 2, 10));
+    assert_eq!(1, min!(2, 10, 1));
+    assert_eq!(1, min!(10, 1, 2));
+    assert_eq!(1, min!(2, 10, 1, 2, 5, 8, 7));
   }
 }
